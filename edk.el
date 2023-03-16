3e1f6b12ccdd3b3f1d893880ef26fc9bad31eb74
Kinneyzhang <kinneyzhang666@gmail.com>
2023-03-16 21:48:50 +0800

    update

--- edk.el
+++ edk.el
@@ -11,4 +11,16 @@
   '("daily view" summary type "daily")
   )
 
+
+;;; window management
+;; (setq truncate-partial-width-windows 50)
+;; (window-total-width)
+
+;;; common db functions
+;; support multifly type of database.
+
+;;; common ewoc functions
+;;; customize views
+;;; built-in time views
+
 (provide 'edk)
